e2178ea4b8564b6ca7f650ee3dcee640b12d0ea4
David <davidlucaspereirapinto06@gmail.com>
2026-07-26 21:44:28 -0300

    Main script

--- Recognition/Directory/janus.py
+++ Recognition/Directory/janus.py
@@ -2,42 +2,71 @@
 
 import requests
 from concurrent.futures import ThreadPoolExecutor 
-import sys
 import pyfiglet
+import argparse
+import time
 
-URL = sys.argv[1]
-WORDLIST = sys.argv[2]
+# Args 
+parser = argparse.ArgumentParser(description="Directory Enumerator")
+parser.add_argument("-u", "--url", help="Set a url here. Example: http://example.com", required=True)
+parser.add_argument("-w", "--wordlist", help="Set a wordlist here. Example: /home/user/Wordlists/small.txt", required=True)
+parser.add_argument("-t", "--threads", type=int, help="Threads to use. Default: 20", default=20, required=False)
+parser.add_argument("-T", "--timeout", type=float, help="Set the timeout for requests. Default: 2", default=2, required=False)
+
+args = parser.parse_args()
+
+# Global Variables
 BANNER = pyfiglet.figlet_format("Janus Enumeration")
-banner = f"{"="*30}{BANNER}{"="*100}"
+URL = args.url
+WORDLIST = args.wordlist
+THREADS = args.threads
+TIMEOUT = args.timeout
+
+# Variables
+banner = "="*30 + BANNER + "="*100
 author = """
 Author: d4vidlinux
 Copyright (c) 2026 d4vidlinux"""
+session = requests.Session()
 
-with open(WORDLIST) as f:
-    wordlist = set([line.strip() for line in f])
+def main():
+    # Wordlist parser
+    with open(WORDLIST) as f:
+        wordlist = set([line.strip() for line in f])
 
-def enumeration(url, word):
-    try:
-        
-        if url[-1] == "/":
+    # Main Function
+    def enumeration(url, word):
+        try:
+            
+            if not url.endswith("/"):
+                url += "/"
+
+            r = session.get(url+word, timeout=TIMEOUT)
+            result = f"[+] {url}{word} - {r.status_code}"
+
+            if r.status_code != 404:
+                print(result)
+
+
+        except requests.RequestException:
             pass
-        else:
-            url += "/"
 
-        r = requests.get(url+word)
-        result = f"[+] {url}{word} - {r.status_code}"
+    # Execution Time
+    begin = time.perf_counter()
 
-        if r.status_code != 404:
-            print(result)
+    # Introduction
+    print(banner+author)
+    print("\n")
 
+    # Threads
+    with ThreadPoolExecutor(max_workers=THREADS) as TPE:
+        TPE.map(lambda word: enumeration(URL, word), wordlist)
+        
 
-    except requests.RequestException:
-        pass
+    final = time.perf_counter()
 
-print(banner+author)
-print("\n")
+    # Final
+    print(f"\nFinished in {final - begin:.2f} seconds\n")
 
-with ThreadPoolExecutor(max_workers=20) as TPE:
-    TPE.map(lambda word: enumeration(URL, word), wordlist)
-
-print("\n")
+if __name__ == "__main__":
+    main()
